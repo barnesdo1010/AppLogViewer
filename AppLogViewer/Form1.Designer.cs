67caf7fc57fca266fedb70688654154fbc3dd23a
David Barnes <david.o.barnes@timkensteel.com>
2021-11-22 16:41:13 -0500

    Add project files.

---
+++ AppLogViewer/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace AppLogViewer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnGetAll = new System.Windows.Forms.Button();
+            this.btnGetLogs = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.btnStartPolling = new System.Windows.Forms.Button();
+            this.btnStopPolling = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 78);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(1014, 731);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnStopPolling);
+            this.groupBox1.Controls.Add(this.btnStartPolling);
+            this.groupBox1.Controls.Add(this.btnGetAll);
+            this.groupBox1.Controls.Add(this.btnGetLogs);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(1014, 48);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Select Executable";
+            // 
+            // btnGetAll
+            // 
+            this.btnGetAll.Location = new System.Drawing.Point(458, 17);
+            this.btnGetAll.Name = "btnGetAll";
+            this.btnGetAll.Size = new System.Drawing.Size(75, 23);
+            this.btnGetAll.TabIndex = 3;
+            this.btnGetAll.Text = "Get All";
+            this.btnGetAll.UseVisualStyleBackColor = true;
+            this.btnGetAll.Click += new System.EventHandler(this.btnGetAll_Click);
+            // 
+            // btnGetLogs
+            // 
+            this.btnGetLogs.Location = new System.Drawing.Point(364, 17);
+            this.btnGetLogs.Name = "btnGetLogs";
+            this.btnGetLogs.Size = new System.Drawing.Size(75, 23);
+            this.btnGetLogs.TabIndex = 2;
+            this.btnGetLogs.Text = "Get Logs";
+            this.btnGetLogs.UseVisualStyleBackColor = true;
+            this.btnGetLogs.Click += new System.EventHandler(this.btnGetLogs_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(6, 19);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(341, 21);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // btnStartPolling
+            // 
+            this.btnStartPolling.Location = new System.Drawing.Point(914, 17);
+            this.btnStartPolling.Name = "btnStartPolling";
+            this.btnStartPolling.Size = new System.Drawing.Size(75, 23);
+            this.btnStartPolling.TabIndex = 4;
+            this.btnStartPolling.Text = "Start Polling";
+            this.btnStartPolling.UseVisualStyleBackColor = true;
+            this.btnStartPolling.Click += new System.EventHandler(this.btnStartPolling_Click);
+            // 
+            // btnStopPolling
+            // 
+            this.btnStopPolling.Location = new System.Drawing.Point(808, 17);
+            this.btnStopPolling.Name = "btnStopPolling";
+            this.btnStopPolling.Size = new System.Drawing.Size(75, 23);
+            this.btnStopPolling.TabIndex = 5;
+            this.btnStopPolling.Text = "Stop Polling";
+            this.btnStopPolling.UseVisualStyleBackColor = true;
+            this.btnStopPolling.Click += new System.EventHandler(this.btnStopPolling_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1038, 822);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnGetAll;
+        private System.Windows.Forms.Button btnGetLogs;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button btnStopPolling;
+        private System.Windows.Forms.Button btnStartPolling;
+    }
+}
+
---
+++ AppLogViewer/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows.Forms;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace AppLogViewer
+{
+    public partial class Form1 : Form, IDisposable
+    {
+        string connectionString = @"Data Source=hsphmltsql1;Initial Catalog=HSPMelt;Persist Security Info=True;User ID=HSPMelt_rw_datauser;Password=HSPMelt_rw_datauser";
+        string executableName = null;
+        string query = null;
+        DispatcherTimer StartPolling = new DispatcherTimer();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            GetData();
+        }
+
+        private void GetData()
+        {
+            query = $"SELECT TOP 1000 [DateTimeStamp], [ExecutableName], [ModuleName], [MessageText] " +
+                    $"FROM ApplicationLog " +
+                    $"order by DateTimeStamp desc";
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                SqlDataAdapter adapter = new SqlDataAdapter(query, conn);
+
+                DataTable data = new DataTable();
+                adapter.Fill(data);
+
+                dataGridView1.DataSource = data;
+            }
+
+            query = "SELECT DISTINCT ExecutableName " +
+                    "FROM [HSPMelt].[dbo].[ApplicationLog] " +
+                    "ORDER BY ExecutableName asc ";
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand(query, conn);
+                SqlDataReader DR = cmd.ExecuteReader();
+
+                while (DR.Read())
+                {
+
+                    comboBox1.Items.Add(DR[0]);
+
+                }
+            }
+
+        }
+
+        private void btnGetLogs_Click(object sender, EventArgs e)
+        {
+            query = $"SELECT TOP 1000 [DateTimeStamp], [ExecutableName], [ModuleName], [MessageText] " +
+                    $"FROM ApplicationLog " +
+                    $"Where ExecutableName = '{executableName}'" +
+                    $"order by DateTimeStamp desc";
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                SqlDataAdapter adapter = new SqlDataAdapter(query, conn);
+
+                DataTable data = new DataTable();
+                adapter.Fill(data);
+
+                dataGridView1.DataSource = data;
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            executableName = comboBox1.SelectedItem.ToString();
+        }
+
+        private void btnGetAll_Click(object sender, EventArgs e)
+        {
+            query = $"SELECT TOP 1000 [DateTimeStamp], [ExecutableName], [ModuleName], [MessageText] " +
+                    $"FROM ApplicationLog " +
+                    $"order by DateTimeStamp desc";
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                SqlDataAdapter adapter = new SqlDataAdapter(query, conn);
+
+                DataTable data = new DataTable();
+                adapter.Fill(data);
+
+                dataGridView1.DataSource = data;
+            }
+        }
+
+        private void btnStartPolling_Click(object sender, EventArgs e)
+        {
+            StartPolling.Interval = System.TimeSpan.FromMilliseconds(5000);
+            StartPolling.Tick += new System.EventHandler(StartPolling_Tick);
+            StartPolling.Start();
+        }
+
+        private void StartPolling_Tick(object sender, EventArgs e)
+        {
+            StartPolling.Stop();
+            GetData();
+            StartPolling.Start();
+        }
+
+        private void btnStopPolling_Click(object sender, EventArgs e)
+        {
+            StartPolling.Stop();
+        }
+    }
+}
